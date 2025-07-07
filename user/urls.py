3e6650d0cedbaf92b7e102832f0bb953fdc28b24
aliakbarsobhanpoor <aliakbarsobhanpoor@gmail.com>
2025-07-08 01:04:14 +0330

    BUG FUTURE * REFACTOR make base.html and make base of templates. also create dashboard page.

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path('storekeeper/login/', views.StorekeeperLoginView.as_view(), name='storekeeper-login-view'),
+    path('dashboard/', views.DashboardView.as_view(), name='dashboard-view'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import login
+from django.shortcuts import render
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-
+from django.views import View
 from user.forms import StorekeeperLoginForm
 
 
@@ -11,8 +12,14 @@ from user.forms import StorekeeperLoginForm
 class StorekeeperLoginView(FormView):
     template_name = "users/storekeeper_login.html"
     form_class = StorekeeperLoginForm
-    success_url = reverse_lazy("warehouse:dashboard")
+    success_url = reverse_lazy("dashboard-view")
 
     def form_valid(self, form):
         login(self.request, form.storekeeper)
         return super().form_valid(form)
+
+
+class DashboardView(View):
+    def get(self, request):
+        context = {}
+        return render(request, "dashboard/main.html", context)
