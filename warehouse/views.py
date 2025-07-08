701e7a1876575c44ab6bbb513c1544bcb5750f36
aliakbarsobhanpoor <aliakbarsobhanpoor@gmail.com>
2025-07-08 15:49:29 +0330

    BUG FUTURE * REFACTOR create creation link custom .

--- warehouse/views.py
+++ warehouse/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views.generic import ListView
 from django_tables2 import SingleTableView
 
 from base.views import StoreKeeperViLoginRequiredMixin
 from warehouse.models import Product
 from warehouse.tables import ProductTable
-
-
-# Create your views here.
 
 class ProductListView(StoreKeeperViLoginRequiredMixin ,SingleTableView, ListView):
     model = Product
@@ -17,3 +14,13 @@ class ProductListView(StoreKeeperViLoginRequiredMixin ,SingleTableView, ListView
 
     def get_queryset(self):
         return Product.objects.all().order_by("created_at")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["creation_link"] = {
+            "title": "ایجاد محصول جدید",
+            "link": reverse("dashboard-view")
+        }
+        context["title"] = "لیست محصولات"
+        return context
+
