9b1ac1ba15fe8d253d4c1ac1adceee006b90dd77
aliakbarsobhanpoor <aliakbarsobhanpoor@gmail.com>
2025-07-09 01:46:12 +0330

    BUG FUTURE * REFACTOR create base table for the code. this is the main one. and the best one.

---
+++ base/tables.py
@@ -0,0 +1,24 @@
+import django_tables2 as tables
+
+class RowNumberColumn(tables.Column):
+    def render(self, value, **kwargs):
+        bound_row = kwargs.get("bound_row")
+        return bound_row.row_counter + 1
+
+class BaseTable(tables.Table):
+    row_number = RowNumberColumn(empty_values=(),verbose_name="شماره ردیف", orderable=False)
+    operations = tables.Column(empty_values=(), verbose_name="عملیات", orderable=False)
+    created_at_by = tables.Column(empty_values=(), verbose_name="تاریخ ایجاد / ایجاد کننده", orderable=False)
+    updated_at_by = tables.Column(empty_values=(), verbose_name="تاریخ ویرایش / ویرایش کننده", orderable=False)
+
+    def render_created_at_by(self, value, **kwargs):
+        record = kwargs.get("record")
+        return f"""
+            {record.created_at.strftime("%Y-%m-%d %H:%M:%S")} , {record.created_by}
+        """
+
+    def render_updated_at_by(self, value, **kwargs):
+        record = kwargs.get("record")
+        return f"""
+            {record.updated_at.strftime("%Y-%m-%d %H:%M:%S")} , {record.created_by}
+        """
--- warehouse/tables.py
+++ warehouse/tables.py
@@ -1,18 +1,21 @@
 import django_tables2 as tables
-
+from django.utils.safestring import mark_safe
+from django.shortcuts import reverse
 from base.functions import thousand_separators
+from base.tables import BaseTable
 from .functions import get_available_stock_level
 from .models import Product
 
 
-class ProductTable(tables.Table):
+class ProductTable(BaseTable):
     stock_level = tables.Column(empty_values=(), verbose_name="موجودی انبار", orderable=False)
 
     class Meta:
         model = Product
         template_name = "django_tables2/bootstrap4.html"
         fields = (
-            "name", "price_label", "shell_item_count", "stock_level",
+            "row_number", "name", "price_label", "shell_item_count", "stock_level", 'created_at_by', 'updated_at_by',
+            'operations',
         )
 
     def render_price_label(self, value, **kwargs):
@@ -20,3 +23,9 @@ class ProductTable(tables.Table):
 
     def render_stock_level(self, record, **kwargs):
         return get_available_stock_level(record.id, None)
+
+    def render_operations(self, record, **kwargs):
+        # language=html
+        return mark_safe("""
+            <a href='{}'>ویرایش</a>
+        """.format(reverse("product-update-view", args=[record.id])))
